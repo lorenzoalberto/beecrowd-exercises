855965d879f03a89ba45d7369a1196533d622c0f
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-06-12 11:00:08 -0300

    #1003 resolvido

---
+++ iniciante/1003.js
@@ -0,0 +1,9 @@
+var input = require("fs").readFileSync("/dev/stdin", "utf8");
+var lines = input.split("\n");
+
+let A = parseInt(lines[0]);
+let B = parseInt(lines[1]);
+
+const X = A + B;
+
+console.log(`SOMA = ${X}`);
