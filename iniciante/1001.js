4a0bf6d83102934292ed039231e4c2abe3909730
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-06-12 13:11:05 -0300

    #1001 melhorado

--- iniciante/1001.js
+++ iniciante/1001.js
@@ -1,11 +1,6 @@
 var input = require("fs").readFileSync("/dev/stdin", "utf8");
 var lines = input.split("\n");
 
-// essa linha não faz nada, o correto seria a abaixo: - limpando os espaços em branco e dividindo a string em linhas
-input.trim().split("\n");
-// var lines = input.trim().split("\n");
-
-// -- parseInt() converte a string para um número inteiro, não precisa de Number()
 let A = parseInt(lines[0]);
 let B = parseInt(lines[1]);
 
