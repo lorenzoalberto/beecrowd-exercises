40a48eb044ed6924bffa1a22a174737111db22a2
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-06-30 10:18:12 -0300

    #1004 resolvido

---
+++ iniciante/typescript/1004.ts
@@ -0,0 +1,9 @@
+var input = require("fs").readFileSync("/dev/stdin", "utf8");
+var lines = input.split("\n");
+
+let A: number = parseInt(lines[0]);
+let B: number = parseInt(lines[1]);
+
+const X: number = A * B;
+
+console.log(`PROD = ${X}`);
